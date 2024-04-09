fb5974e23b3158bedafac27084ede2ec30d5f4d4
Jae <121826115+saikendream@users.noreply.github.com>
2024-04-09 14:23:39 -0300

    Fixed caret bug on INPUT DIV

--- script.js
+++ script.js
@@ -2,11 +2,22 @@
 
 const textField = document.querySelector("#digits");
 
+// FORCE CARET TO THE END OF INPUT
+// https://phuoc.ng/collection/html-dom/move-the-cursor-to-the-end-of-a-content-editable-element/
+
+function forceCaret () {
+    const range = document.createRange(); // Sets the RANGE
+    const selection = window.getSelection(); // Watches the selection
+    range.setStart(textField, textField.childNodes.length); // Sets the start and end
+    range.collapse(true); // Sets the endpoint = start point
+    selection.removeAllRanges(); // Delete all ranges
+    selection.addRange(range); // Sets the new range to the SELECTION
+};
+
 // IGNORE WHITESPACES AND LETTERS
 
 textField.addEventListener("input", function(e) {
-    console.log("onlyNums has been fired");
-    console.log("input is " + textField.textContent);
-    let pattern = /[\sa-zA-Z]+/g;
-    textField.textContent = textField.textContent.replace(pattern, '');
+    let pattern = /[\sa-zA-Z]+/g; // Sets the pattern to grab every alphabet and space character
+    textField.textContent = textField.textContent.replace(pattern, ''); // Erases what matches PATTERN
+    forceCaret();
 });
