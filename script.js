ad65368392ac7c3c193f890426c45046e24cdeaa
Jae <121826115+saikendream@users.noreply.github.com>
2024-04-09 11:56:11 -0300

    Code adapted to support contenteditable DIV instead of INPUT

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const textField = document.querySelector("#digits");
 // IGNORE WHITESPACES AND LETTERS
 
 textField.addEventListener("input", function(e) {
+    console.log("onlyNums has been fired");
+    console.log("input is " + textField.textContent);
     let pattern = /[\sa-zA-Z]+/g;
-    this.value = this.value.replace(pattern, '');
+    textField.textContent = textField.textContent.replace(pattern, '');
 });
